bda52654822801b3fb4f27752ab6eebf5f094d1a
zuiyu1998 <1542844298@qq.com>
2024-06-18 16:21:09 +0800

    feat: rfcs 实体的动画轨道

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct EntityTrack {}
+
+pub fn update_animation() {}
+
+pub struct BevyNextAnimationPlugin;
+
+impl Plugin for BevyNextAnimationPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, update_animation);
+    }
 }
